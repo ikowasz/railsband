29c456010974f4f0d957c60767fe6eb7ac0dc3fa
ikowasz <dev.ikowasz@gmail.com>
2025-09-21 10:26:05 +0200

    song scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_21_071225) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_21_082344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
+  create_table "songs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
