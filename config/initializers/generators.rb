64ad027caf1c376bb950f5dea1b1ce01e69c6933
ikowasz <dev.ikowasz@gmail.com>
2025-09-21 13:57:24 +0200

    default now for timestamps

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,3 +1,15 @@
 Rails.application.config.generators do |g|
   g.orm :active_record, primary_key_type: :uuid
 end
+
+module ActiveRecord::ConnectionAdapters
+  class TableDefinition
+    alias :base_timestamps :timestamps
+
+    def timestamps(**options)
+      options[:default] = -> { "NOW()" }
+
+      base_timestamps(**options)
+    end
+  end
+end
